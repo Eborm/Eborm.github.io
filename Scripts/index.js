5c1205cdaf6ddbe992e5696ab8afae152b80079c
Eborm <bramwesel@gmail.com>
2023-10-02 22:47:37 +0200

    Updated it so it doens't flikker

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,19 +1,32 @@
 const triggerElement = document.querySelector('.Projects');
 const elementToShow = document.querySelector('.Plang');
 
+let isMouseInsidePlang = false;
+
+function showElement() {
+    elementToShow.style.visibility = 'visible';
+}
+
+function hideElement() {
+    if (!isMouseInsidePlang) {
+        elementToShow.style.visibility = 'hidden';
+    }
+}
+
 // Add a hover event listener to the trigger element
-triggerElement.addEventListener('mouseenter', () => {
-    // Make the element to show visible
-    elementToShow.style.visibility = 'visible';
-});
+triggerElement.addEventListener('mouseenter', showElement);
 
+// Add a mouseleave event listener to hide the element when not hovering over the trigger element
+triggerElement.addEventListener('mouseleave', hideElement);
+
+// Add a mouseenter event listener to track when the mouse enters the Plang element
 elementToShow.addEventListener('mouseenter', () => {
-    // Keep the element visible
-    elementToShow.style.visibility = 'visible';
+    isMouseInsidePlang = true;
+    showElement(); // Show the element when the mouse enters
 });
 
-// Add a mouseleave event listener to hide the element when not hovering
-triggerElement.addEventListener('mouseleave', () => {
-    // Hide the element
-    elementToShow.style.visibility = 'hidden';
+// Add a mouseleave event listener to track when the mouse leaves the Plang element
+elementToShow.addEventListener('mouseleave', () => {
+    isMouseInsidePlang = false;
+    hideElement(); // Hide the element when the mouse leaves if not already inside
 });
