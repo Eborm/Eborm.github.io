98ad551d54876a733ade43a28d792728510aee50
Eborm <bramwesel@gmail.com>
2023-10-17 00:28:09 +0200

    changed the hover to a toggle

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,44 +3,22 @@ const elementToShow = document.querySelector('.Plang');
 const elementTohide = document.querySelector('.DropDownIcon');
 const elementUp = document.querySelector('.Up');
 
-let isMouseInsidePlang = false;
-
-function showElements() {
-    elementToShow.style.visibility = 'visible';
-    elementToShow.style.opacity = '1';
-    elementTohide.style.visibility = 'hidden';
-    elementTohide.style.opacity = '0';
-    elementUp.style.visibility = 'visible';
-    elementUp.style.opacity = '1';
-}
-
-function hideElements() {
-    if (!isMouseInsidePlang) {
+// Add a click event listener to the trigger element
+triggerElement.addEventListener('click', () => {
+    // Toggle the element's visibility on each click
+    if (elementToShow.style.visibility === 'hidden') {
+        elementToShow.style.opacity = '1';
+        elementTohide.style.opacity = '0';
+        elementUp.style.opacity = '1';
+        elementToShow.style.visibility = 'visible';
+        elementTohide.style.visibility = 'hidden';
+        elementUp.style.visibility = 'visible';
+    } else {
         elementToShow.style.opacity = '0';
         elementTohide.style.opacity = '1';
         elementUp.style.opacity = '0';
-        setTimeout(() => {
-            elementToShow.style.visibility = 'hidden';
-            elementTohide.style.visibility = 'visible';
-            elementUp.style.visibility = 'hidden';
-        }, 500); // Adjust the delay to match the transition duration
+        elementToShow.style.visibility = 'hidden';
+        elementTohide.style.visibility = 'visible';
+        elementUp.style.visibility = 'hidden';
     }
-}
-
-// Add a hover event listener to the trigger element
-triggerElement.addEventListener('mouseenter', showElements);
-
-// Add a mouseleave event listener to hide the elements when not hovering over the trigger element
-triggerElement.addEventListener('mouseleave', hideElements);
-
-// Add a mouseenter event listener to track when the mouse enters the Plang element
-elementToShow.addEventListener('mouseenter', () => {
-    isMouseInsidePlang = true;
-    showElements(); // Show the elements when the mouse enters
-});
-
-// Add a mouseleave event listener to track when the mouse leaves the Plang element
-elementToShow.addEventListener('mouseleave', () => {
-    isMouseInsidePlang = false;
-    hideElements(); // Hide the elements when the mouse leaves if not already inside
 });
