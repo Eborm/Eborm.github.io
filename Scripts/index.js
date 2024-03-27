057b5617a0afa7d72e267b8bad8b835cabde4c74
Eborm <bramwesel@gmail.com>
2024-03-27 11:05:09 +0100

    Changed to whole page

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,24 +1,10 @@
-const triggerElement = document.querySelector('.Projects');
-const elementToShow = document.querySelector('.Plang');
-const elementTohide = document.querySelector('.DropDownIcon');
-const elementUp = document.querySelector('.Up');
-
-// Add a click event listener to the trigger element
-triggerElement.addEventListener('click', () => {
-    // Toggle the element's visibility on each click
-    if (elementToShow.style.visibility === 'hidden') {
-        elementToShow.style.opacity = '1';
-        elementTohide.style.opacity = '0';
-        elementUp.style.opacity = '1';
-        elementToShow.style.visibility = 'visible';
-        elementTohide.style.visibility = 'hidden';
-        elementUp.style.visibility = 'visible';
-    } else {
-        elementToShow.style.opacity = '0';
-        elementTohide.style.opacity = '1';
-        elementUp.style.opacity = '0';
-        elementToShow.style.visibility = 'hidden';
-        elementTohide.style.visibility = 'visible';
-        elementUp.style.visibility = 'hidden';
-    }
-});
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
